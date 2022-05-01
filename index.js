cafc617ae08521a695a8740faee7a67c7d48f47c
nightfury <linjay0208@gmail.com>
2022-05-01 10:30:53 +0800

    update

--- index.js
+++ index.js
@@ -12,15 +12,81 @@ app.use(cors());
 
 const Mint = require('./schema/mint');
 const { updateMetaData } = require('./lib/index.js');
+const { nextTick } = require('process');
+
+app.get('/', (req, res) => {
+    res.send('backend is working well...');
+});
+
+app.get('/staked:id', (req, res, next) => {
+    const staker = req.params.id;
+    Mint.find({ staker }, (err, doc) => {
+        if (err) return next(err);
+        if (doc.length > 0) res.status(200).send(doc.map((doc) => doc.mintAddr));
+        else res.status(404).send({ err: 'Null' });
+    });
+});
+
+app.get('/reset:id', (req, res, next) => {
+    const staker = req.params.id;
+    Mint.findOneAndRemove({ staker }, (err, doc) => {
+        if (err) return next(err);
+        if (doc) res.status(200).send(doc.mintAddr);
+        else res.status(404).send({ err: 'Null' });
+    });
+});
+
+app.post('/unstaked', (req, res, next) => {
+    const { staker, mintAddr } = req.body;
+    Mint.findOne({ mintAddr }, (err, doc) => {
+        if (Date.now() - doc.createdAt <= 3600 * 24 * 7 * 1000) {
+            return res.status(400).send("Not yet")
+        }
+    });
+    Mint.findOneAndRemove({ mintAddr }, (err, doc) => {
+        if (err) return next(err);
+        if (doc) {
+            res.status(200).send(doc.mintAddr);
+        } else {
+            res.status(400).send('no data');
+        }
+    });
+});
+
+app.post('/staked', (req, res, next) => {
+    const { staker, mintAddr } = req.body;
+
+    // const mint = new Mint({ mintAddr: req.body.mintAddr });
+    Mint.findOne({ mintAddr }, (err, doc) => {
+        if (err) return next(err);
+        if (doc) {
+            return res.status(400).send({ err: 'Duplicate NFT' });
+        } else {
+            const mint = new Mint({
+                staker,
+                mintAddr: mintAddr,
+            });
+            mint.save((err, docs) => {
+                if (err) return err;
+                res.status(200).send(docs);
+            });
+        }
+    });
+    // mint.save().then((a) => console.log('db :', a));
+});
 
 app.post('/update', (req, res) => {
     // console.log(web3_js_1.Keypair.fromSecretKey(new Uint8Array(process.env.KEY_150.split(','))));
-    updateMetaData(req.body.mintAddr);
+    try {
+        updateMetaData(req.body.mintAddr);
+    } catch (err) {
+        res.status(400).send(err);
+    }
     res.send('Successfully updated');
     // const mint = new Mint({ mintAddr: req.body.mintAddr });
     // mint.save().then((a) => console.log('db :', a));
 });
-const PORT = process.env.PORT || 8008
+const PORT = process.env.PORT || 8008;
 // app.get('/get');
 
 var server = app.listen(PORT, function () {
--- schema/mint.js
+++ schema/mint.js
@@ -1,11 +1,17 @@
+const { timeStamp } = require('console');
 const mongoose = require('mongoose');
 
 const MintSchema = new mongoose.Schema(
     {
-        mintAddr: {
+        staker: {
             type: String,
             required: true,
         },
+        mintAddr: {
+            type: String,
+            required: true
+        }
     },
+    { timestamps: true },
 );
 module.exports = mongoose.model('Mint', MintSchema);
